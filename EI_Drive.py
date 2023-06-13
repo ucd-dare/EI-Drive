7247455f7a518c554a9ebfcde9a5ba62b07aebcd
Dechen <gaodechen@live.cn>
2023-06-13 13:49:42 -0700

    Updated requirements.txt

--- EI_Drive.py
+++ EI_Drive.py
@@ -6,40 +6,15 @@ Script to run different scenarios.
 # Author: Wei Shao <phdweishao@gmail.com>
 # License: TDG-Attribution-NonCommercial-NoDistrib
 
-import argparse
 import importlib
 import sys
 import hydra
-import time
 from omegaconf import DictConfig, OmegaConf
 from omegaconf import OmegaConf
 
 from EIdrive.scenario_testing.utils.yaml_utils import load_yaml
 from EIdrive.version import __version__
 import os
-
-
-def arg_parse():
-    parser = argparse.ArgumentParser(description="EI-Drive scenario runner.")
-    parser.add_argument('-t', "--test_scenario", required=True, type=str,
-                        help='Define the name of the scenario you want to test. The given name must'
-                             'match one of the testing scripts(e.g. single_2lanefree_carla) in '
-                             'EIdrive/scenario_testing/ folder'
-                             ' as well as the corresponding yaml file in EIdrive/scenario_testing/config_yaml.')
-    parser.add_argument("--record", action='store_true', help='whether to record and save the simulation process to'
-                                                              '.log file')
-    parser.add_argument("--apply_ml",
-                        action='store_true',
-                        help='whether ml/dl framework such as sklearn/pytorch is needed in the testing. '
-                             'Set it to true only when you have installed the pytorch/sklearn package.')
-    parser.add_argument('-v', "--version", type=str, default='0.9.11',
-                        help='Specify the CARLA simulator version, default'
-                             'is 0.9.11, 0.9.12 is also supported.')
-    parser.add_argument('--edge', action='store_true',
-                        help='whether to enable edge')
-
-    opt = parser.parse_args()
-    return opt
 
 
 @hydra.main(version_base=None, config_path='EIdrive/scenario_testing/config_yaml', config_name='default')
