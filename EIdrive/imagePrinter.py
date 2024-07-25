c489f8159d2641b6fe1d45d211de9f78e8c7c678
edheadd <ebexie@gmail.com>
2024-07-25 15:41:18 -0700

    polished coop 1-4

--- EIdrive/imagePrinter.py
+++ EIdrive/imagePrinter.py
@@ -2,13 +2,11 @@ import cv2
 import pygame
 
 def save_CV(image, folder, count, ID):
-    return
     filepath = f"/home/junshan/imageTest/{folder}"
     #if count%10 == 0:
     cv2.imwrite(f"{filepath}/{ID}_image{count}.jpg", image)
 
 def save_pygame(surface, count):
-    return
     filepath = f"/home/junshan/imageTest/pygame"
     #if count % 10 == 0:
     pygame.image.save(surface, f"{filepath}/image{count}.jpg")
